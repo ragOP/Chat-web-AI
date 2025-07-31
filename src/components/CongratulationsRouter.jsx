8d102f3d977c30cfa0006c6a7a41edb4dc57be31
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2025-07-31 20:42:28 +0530

    ./components/

--- src/components/CongratulationsRouter.jsx
+++ src/components/CongratulationsRouter.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { CongoPage } from "./CongoPage";
-import { CongoPage2 } from "./CongoPage2";
+import  CongoPage  from "./CongoPage";
+import  CongoPage2  from "./CongoPage2";
 
 
 const CongratulationsRouter = () => {
